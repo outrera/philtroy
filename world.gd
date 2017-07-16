ea9acc83eb3b0e0034478464fb580e78c36f19ca
CultOfApe <ted.project.bcn@gmail.com>
2017-07-16 19:14:32 +0200

    Implemented simple turn and move towards functionality
    still some polishing needed. Player should move towards NPC before talking. If mouseclick is too close to player, player won´t play fully. Turn speed should be relative to distance to target (with min and max speeds), and if close to target, player should turn without moving. Not hard to do, just need to take the time.

--- world.gd
+++ world.gd
@@ -1,9 +1,9 @@
 extends Node
 
+var value = 0
 var game_data = {}
 var curpos
 var startpos
-var target
 var direction
 var player_speed = 7
 var click_pos
@@ -22,7 +22,6 @@ var dialogue_running = false
 var phone_opened = false
 
 #get some nodes for easy access
-onready var player = get_node("player")
 onready var effect_ui_hover = get_node("effects/ui_hover")
 onready var effect_ui_hide_show = get_node("effects/ui_hide_show")
 onready var label_hotspot = get_node("ui_elements/Label_hotspot")
@@ -32,6 +31,24 @@ onready var dialog_panel = load("res://asset scenes/dialog_window.tscn")
 onready var reply_button = load("res://asset scenes/reply.tscn")
 
 onready var viewsize = get_viewport().get_rect().size
+
+onready var player_viewport_pos = get_node("camera").unproject_position(get_node("player").get_global_transform().origin)
+onready var target_viewport_pos = Vector3(0,0,0)
+
+#onready var player_label = get_node("player/player_label")
+#onready var target_label = get_node("target/target_label")
+	
+onready var player = get_node("player")
+onready var target = Vector3(0,0,0)
+onready var helper = get_node("player/rotation_helper/Position3D")
+	
+onready var target_pos = Vector3(0,0,0)
+onready var player_pos = player.get_global_transform().origin
+onready var helper_pos = helper.get_global_transform().origin	
+
+var playerFacing
+
+var movement = Vector3()
 
 #set character start dialogues
 var ellie = {"dialogue": "res://dialogue/ellie_start.json", "branch": "a"}
@@ -52,12 +69,11 @@ func _process(delta):
 	if Input.is_action_pressed("ui_quit"):
 		get_tree().quit()
 	#if dialogue is running and we press ui_exit, exit dialogue and delete dialogue nodes
-	if dialogue_running == true:
-		if Input.is_action_pressed("ui_exit"):
+	if Input.is_action_pressed("ui_exit"):
+		if dialogue_running == true:
 			kill_dialogue()
-	if phone_opened == true:
-		if Input.is_action_pressed("ui_exit"):
-			no_move_on_click = false
+		if phone_opened == true:	
+			no_move_on_click = false #works same as blocking_ui, unify
 			phone_opened = false
 			ui_hide_show(get_node("ui_elements/ui_phone"), Vector2(0,1310), Tween.TRANS_QUAD, Tween.EASE_OUT)
 			unhide_ui_icons()
@@ -67,10 +83,28 @@ func _fixed_process(delta):
 	#TODO: make player turn smoother
 	if is_moving:
 		if !blocking_ui:
-			player.move(direction * player_speed * delta)
-			curpos = player.get_global_transform().origin
-			if curpos.distance_to(startpos) > distance_to_target:
-				is_moving = false
+#			player.move(direction * player_speed * delta)
+#			curpos = player.get_global_transform().origin
+#			if curpos.distance_to(startpos) > distance_to_target:
+#				is_moving = false
+				
+			#new movement code
+			if player_pos.distance_to(target_pos) > 5:
+
+				var t = player.get_transform()
+				var lookDir = target_pos - player_pos
+				var rotTransform = t.looking_at(target_pos,Vector3(0,1,0))
+				var thisRotation = Quat(t.basis).slerp(rotTransform.basis,value*0.1)
+				value += delta
+				if value>1:
+    				value = 1
+				player.set_transform(Transform(thisRotation,t.origin))	
+				player_pos = player.get_global_transform().origin
+				helper_pos = helper.get_global_transform().origin	
+				playerFacing = (helper_pos - player_pos).normalized()
+				player.move(playerFacing*delta*3)
+			else:
+				pass
 
 func _input(event):
 	if hover_node and hover_node.get_name() == "phone":	
@@ -84,12 +118,23 @@ func _input(event):
 func _on_Area_input_event( camera, event, click_pos, click_normal, shape_idx ):
 	if !blocking_ui:
 		if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.pressed and no_move_on_click == false:
-			curpos = player.get_global_transform().origin
-			startpos = curpos
-			player.look_at(click_pos, Vector3(0,1,0))
-			direction = (click_pos - curpos).normalized()
-			distance_to_target = curpos.distance_to(click_pos)
+			#preparing for new movement code
 			is_moving = true
+			player_pos = player.get_global_transform().origin
+			helper_pos = helper.get_global_transform().origin
+			target_pos = click_pos
+			#end preparing
+			
+			#remove and replace with new movement code
+#			curpos = player.get_global_transform().origin
+#			startpos = curpos
+#			#player.look_at(click_pos, Vector3(0,1,0))
+#			var angle = player.get_transform().basis.z.angle_to(click_pos)
+#			player.rotate_y(angle)
+#			print(angle)
+#			direction = (click_pos - curpos).normalized()
+#			distance_to_target = curpos.distance_to(click_pos)
+#			is_moving = true
 	else:
 		#need to add this so player doesn´t move when exiting dialog
 		direction = Vector3(0,0,0)
@@ -139,11 +184,21 @@ func ui_hide_show(gui_node, move_delta, method1, method2):
 	effect_ui_hide_show.interpolate_property (gui_node, "transform/pos", gui_node.get_pos(), gui_node.get_pos() + move_delta, 1, method1, method2)
 	effect_ui_hide_show.start()
 
+#combine two below functions into toggle_ui_icons(delta)
+func hide_ui_icons():
+	for ui_node in get_tree().get_nodes_in_group("UI_icons"):
+		ui_hide_show(ui_node, Vector2(0,200), Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+
+func unhide_ui_icons():
+	for ui_node in get_tree().get_nodes_in_group("UI_icons"):
+		ui_hide_show(ui_node, Vector2(0,-200), Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+
 #dynamically load the json for character dialogue
 func load_json(json, type):
 	var file = File.new();
 	file.open(json[type], File.READ);
 	game_data.parse_json(file.get_as_text())	
+	file.close()
 
 func start_dialogue(json,pos):
 	load_json(json, "dialogue")
@@ -210,14 +265,6 @@ func new_label(labels):
 			node.set_name(lbl)
 			node.connect("reply_selected",self,"_pick_reply",[], CONNECT_ONESHOT)
 			get_node("ui_elements/ui_dialogue").add_child(node)
-
-func hide_ui_icons():
-	for ui_node in get_tree().get_nodes_in_group("UI_icons"):
-		ui_hide_show(ui_node, Vector2(0,200), Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-
-func unhide_ui_icons():
-	for ui_node in get_tree().get_nodes_in_group("UI_icons"):
-		ui_hide_show(ui_node, Vector2(0,-200), Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	
 func kill_dialogue():
 	for x in get_node("ui_elements/ui_dialogue").get_children():
